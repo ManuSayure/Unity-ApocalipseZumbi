dfc4db59d774be16a4495e838adf22e809007de5
Manoela Cassia <manoelacs@ic.ufal.br>
2020-10-09 10:41:05 -0300

    adicionando controller da camera

---
+++ Assets/Scripts/ControllerCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCamera : MonoBehaviour
+{
+    public GameObject Jogador;
+    Vector3 distanciaJogadorCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanciaJogadorCamera = transform.position - Jogador.transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        transform.position = Jogador.transform.position + distanciaJogadorCamera;
+        
+    }
+}
