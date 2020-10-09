47b76a659ddcbdbd572ac06af23eda7e21b5f93e
Manoela Cassia <manoelacs@ic.ufal.br>
2020-10-09 08:55:01 -0300

    adicionando teclas de movimentação no personagem principal

--- Assets/Scripts/ControllerJogador.cs
+++ Assets/Scripts/ControllerJogador.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class ControllerJogador : MonoBehaviour
 {
     // Start is called before the first frame update
+    
    
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward);
+        float eixoX = Input.GetAxis("Horizontal"); // move com as teclas (left e rigth) e  (a , d)
+        float eixoZ= Input.GetAxis("Vertical"); // move com as teclas (up, down) e (s , w)
+        Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
+        transform.Translate(direcao);
         
     }
 }
