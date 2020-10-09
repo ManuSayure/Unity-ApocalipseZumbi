44a7a2c187d08c4fe1e2af8743c047cedf62ce30
Manoela Cassia <manoelacs@ic.ufal.br>
2020-10-09 11:51:58 -0300

    adicionando colisão e sistema de física ao personagem principal

--- Assets/Scripts/ControllerJogador.cs
+++ Assets/Scripts/ControllerJogador.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class ControllerJogador : MonoBehaviour
 {
     public float Velocidade = 10;  
-   
+    Vector3 direcao;
+    
 
-    // Update is called once per frame
+    // Update is called once per frame    
     void Update()
     {
         float eixoX = Input.GetAxis("Horizontal"); // move com as teclas (left e rigth) e  (a , d)
         float eixoZ= Input.GetAxis("Vertical"); // move com as teclas (up, down) e (s , w)
-        Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
-        float tempoPorSegundo = Time.deltaTime; // tempo por segundo para mover um quadradinho no cenário
-        transform.Translate(direcao * Velocidade * tempoPorSegundo );
+        direcao = new Vector3(eixoX, 0, eixoZ);
+         // tempo por segundo para mover um quadradinho no cenário
+        //transform.Translate(direcao * Velocidade * tempoPorSegundo );        
+    
         bool movendo = false;
         if(direcao != Vector3.zero){
             movendo = true;
@@ -24,5 +26,12 @@ public class ControllerJogador : MonoBehaviour
         }
         GetComponent<Animator>().SetBool("Movendo", movendo);
         
+    }
+    void FixedUpdate() {
+        float tempoPorSegundo = Time.deltaTime;
+        Vector3 posicaoJogador = GetComponent<Rigidbody>().position;
+        
+        GetComponent<Rigidbody>().MovePosition
+            ( posicaoJogador +  (direcao * Velocidade *tempoPorSegundo ));
     }
 }
